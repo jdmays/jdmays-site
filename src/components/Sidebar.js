8dd9f63294df0406e21809cc28a0198676ac7dae
JD Mays <jdmays@gmail.com>
2024-06-24 17:39:47 -0500

    Make More Responsive
    Make it not look terrible on small screens and more edits.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -18,59 +18,91 @@ const Sidebar = () => {
         />
       </div>
 
-      <h3 className="text-2xl mx-8 px-8 sm:px-4 sm:mx-4 font-bold text-black">Jim Mays</h3>
+      <h3 className="text-2xl mx-8 px-8 sm:px-4 sm:mx-4 font-bold text-black">
+        Jim Mays
+      </h3>
 
       <section className="mx-8 px-8 sm:px-4 sm:mx-4 md:px-4 md:mx-4 space-y-4">
         <div className="text-lg tracking-wider">JDMays@gmail.com</div>
 
         <div className="border-t border-purple-900 my-4"></div>
 
-        <h4 className="text-xl font-bold">About Me</h4>
-        <p className="text-black">I'm Jim Mays.</p>
-        <p className={"text-block"}>
-          I'm a web application developer that really enjoys Making The Magic.
-          I've been developing web applications since the days of Perl CGI
-          scripts and I've worked with a variety of technologies over the years,
-          but I'm always looking for new challenges.
-        </p>
+        <div className={"hidden sm:block"}>
+          <h4 className="text-xl font-bold">About Me</h4>
+          <p className="text-black">I'm Jim Mays.</p>
+          <p className={"text-block"}>
+            I'm a web application developer that really enjoys Making The Magic.
+            I've been developing web applications since the days of Perl CGI
+            scripts and I've worked with a variety of technologies over the years,
+            but I'm always looking for new challenges.
+          </p>
 
-        <p className={"text-block pb-8"}>
-          This site is a little different than just my resume. After all, why
-          would you need to visit if there wasn't something different. I think
-          of this as my amplified resume. It's a little more information than
-          what you might get off a job application or pdf. I hope you enjoy it.
-        </p>
+          <p className={"text-block pb-8"}>
+            This site is a little different than just my resume. After all, why
+            would you need to visit if there wasn't something different. I think
+            of this as my amplified resume. It's a little more information than
+            what you might get off a job application or pdf. I hope you enjoy it.
+          </p>
+        </div>
 
-        <a href={"/jim_mays_resume.pdf"}
-          className="text-white bg-gradient-to-r from-purple-900 to-indigo-900 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-        >
-          <FontAwesomeIcon icon={faFilePdf} size={"lg"} /> Download Resume
-        </a>
+        <div className="w-1/3">
+          <a href="/jim_mays_resume.pdf"
+             className="flex items-center justify-center bg-gradient-to-r from-purple-900 to-indigo-900 hover:bg-gradient-to-bl
+      focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-3 py-2.5 text-center text-white"
+          >
+            <FontAwesomeIcon icon={faFilePdf} size="lg"/>
+            <span className="hidden md:inline ml-2">Download Resume</span>
+          </a>
+        </div>
 
-        <ul className={"flex space-x-4 py-2 pt-8"}>
-          <li className={"text-gray-600"}>
-            <a
-              href="https://www.linkedin.com/in/jdmays/"
-              aria-label={"LinkedIn"}
-            >
-              <FontAwesomeIcon icon={faLinkedin} size={"2x"} />
+
+        <ul className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 py-2 pt-8">
+          <li className="text-gray-600">
+            <a href="https://www.linkedin.com/in/jdmays/" aria-label="LinkedIn">
+              <FontAwesomeIcon icon={faLinkedin} size="2x"/>
             </a>
           </li>
-          <li className={"text-gray-600"}>
-            <a href="https://github.com/jdmays" aria-label={"Github/jdmays"}>
-              <FontAwesomeIcon icon={faGithub} size={"2x"} />
+          <li className="text-gray-600">
+            <a href="https://github.com/jdmays" aria-label="Github/jdmays">
+              <FontAwesomeIcon icon={faGithub} size="2x"/>
             </a>
           </li>
-          <li className={"text-gray-600"}>
-            <a href="mailto:jdmays@gmail.com" aria-label={"jdmays@gmail.com"}>
-              <FontAwesomeIcon icon={faEnvelope} size={"2x"} />
+          <li className="text-gray-600">
+            <a href="mailto:jdmays@gmail.com" aria-label="jdmays@gmail.com">
+              <FontAwesomeIcon icon={faEnvelope} size="2x"/>
             </a>
           </li>
-          <li className={"text-gray-600 flex items-center"}>
-            <FontAwesomeIcon icon={faReact} size={"2x"} />
-            <span className={"px-1"}>Written with React</span>
+          <li className="text-gray-600 flex items-center">
+            <FontAwesomeIcon icon={faReact} size="2x"/>
+            <span className="px-1 hidden sm:inline">Written with React</span>
           </li>
         </ul>
+
+
+        {/*<ul className={"flex space-x-4 py-2 pt-8"}>*/}
+        {/*  <li className={"text-gray-600"}>*/}
+        {/*    <a*/}
+        {/*      href="https://www.linkedin.com/in/jdmays/"*/}
+        {/*      aria-label={"LinkedIn"}*/}
+        {/*    >*/}
+        {/*      <FontAwesomeIcon icon={faLinkedin} size={"2x"} />*/}
+        {/*    </a>*/}
+        {/*  </li>*/}
+        {/*  <li className={"text-gray-600"}>*/}
+        {/*    <a href="https://github.com/jdmays" aria-label={"Github/jdmays"}>*/}
+        {/*      <FontAwesomeIcon icon={faGithub} size={"2x"} />*/}
+        {/*    </a>*/}
+        {/*  </li>*/}
+        {/*  <li className={"text-gray-600"}>*/}
+        {/*    <a href="mailto:jdmays@gmail.com" aria-label={"jdmays@gmail.com"}>*/}
+        {/*      <FontAwesomeIcon icon={faEnvelope} size={"2x"} />*/}
+        {/*    </a>*/}
+        {/*  </li>*/}
+        {/*  <li className={"text-gray-600 flex items-center"}>*/}
+        {/*    <FontAwesomeIcon icon={faReact} size={"2x"}/>*/}
+        {/*    <span className={"px-1 hidden sm:inline"}>Written with React</span>*/}
+        {/*  </li>*/}
+        {/*</ul>*/}
 
         <div className="border-t border-purple-900 my-4"></div>
       </section>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,72 +1,53 @@
 import React from 'react'
 
 const Home = () => {
-    return (
-      <div className="bg-purple-100 text-gray-800 min-h-screen">
-        <div className="container mx-auto py-10 px-5">
-          <h1 className="text-3xl font-bold mb-5">About Me (The TLDR Version)</h1>
+  return (
+    <div className="bg-purple-100 text-gray-800 min-h-screen">
+      <div className="container mx-auto py-10 px-5">
+        <h1 className="text-3xl font-bold mb-5">About Me (The TLDR Version)</h1>
+        <div className="border-t border-purple-900 my-4"></div>
+        <div>
+          <p className={"block sm:hidden text-block"}>My name is Jim Mays.</p>
+
+          <p className={"block sm:hidden text-block"}>
+            I'm a web application developer that really enjoys Making The Magic.
+            I've been developing web applications since the days of Perl CGI
+            scripts and I've worked with a variety of technologies over the
+            years, but I'm always looking for new challenges.
+          </p>
+
+          <p className="text-md text-gray-600 my-2">
+            If you take a good look at my experience and eduction, you'll notice
+            there are two careers buried in there. For the first twenty years or
+            so I was a Mental Health Counselor and then a healthcare executive.
+            I was always interested in Information Technology and during that
+            time I was also writing programs and scripts to make my job easier.
+          </p>
+          <p className="text-md text-gray-600 my-2">
+            One of the threads that ties it all together is; I'm a creative
+            problem-solver, I love to build things and I absolutely love it when
+            a plan comes together.
+          </p>
+
+          <p className="text-md text-gray-600 my-2">
+            Switching careers takes a lot of work. In order to do it
+            successfully you have to be willing to learn. You have to be willing
+            to listen and you have to like taking on new challenges.
+          </p>
           <div className="border-t border-purple-900 my-4"></div>
-          <div>
 
-            <p className="text-md text-gray-600 my-2">
-              I'm not your typical engineer. In fact, I'll tell you a secret.
-              I'm not an engineer at all. At least not in the way you might be
-              thinking. I'm a problem solver. I'm a creator. I'm a builder. I
-              love it when a plan comes together.
-            </p>
+          <p className="text-md text-gray-600 my-2">
+            When I'm not working I reside in Northern Illinois with my wife and
+            two German Shepherd Dogs. We love spending time outside; hiking,
+            kayaking or fixing things up around the house. One of my latest
+            projects was a chicken coop which I have nicknamed the Chicken
+            Palace due to its opulent construction. Those chickens sure have it good.
+          </p>
 
-            <p className="text-md text-gray-600 my-2">
-              Let's go back in time. Back to the halcyon days of the late 70s.
-            </p>
-
-            <p className="text-md text-gray-600 text-block my-2">
-              In my Algebra class, there was a section set aside for some
-              computers. I was able to just hack away to my delight. No, that's
-              not true. Actually, we weren't allowed to touch the computers.
-              Those were for people in advanced math classes and I was
-              definitely not in an advanced math class.
-            </p>
-
-            <p className="text-md text-gray-600 my-2">
-              At that time, I had a good friend who loved computers. His
-              aspiration was to go work for Apple Computer. (He did and Macs are
-              still a big part of his consulting business.) As for me, I thought
-              computers would never be that big of a deal. Fine for some niche
-              areas but not for most normal people. However, what I really
-              enjoyed was Philosophy and what I thought of as Applied Philosophy
-              but most know it as Psychology.
-            </p>
-
-            <p className="text-md text-gray-600 my-2">
-              When people give career advice they usually say something like,
-              "Find your passion and follow that. If you love what you do you'll
-              never work a day in your life." That kind of advice is fine if
-              there is anything that you're passionate about. What if you are
-              pretty indifferent to most things and just want to do something
-              that isn't too horribly unpleasant? That was me. The career advice
-              I gave to myself and give to others today is this: Most people
-              don't know what they want to do, yet they do know what they hate
-              doing. Just steer away from what you hate doing and eventually
-              you'll find something you like.
-            </p>
-
-            <p className="text-md text-gray-600 my-2">
-              The thing about Psychology, especially when it comes down to
-              applying it to real live people, is that it's a system or a
-              constellation of things. There is never just one thing. I love
-              systems. I think about them a lot; whether it is groups of people,
-              organizations, or a computer system.
-            </p>
-
-            <p className="text-md text-gray-600 my-2">
-              Which brings me back to being a software developer. Code doesn't
-              exist in isolation. It's a whole bunch of things working together.
-              It involves pulling things together and making them work together,
-              much like a story.
-            </p>
-          </div>
+          <div className="border-t border-purple-900 my-4"></div>
         </div>
       </div>
-    );
-}
-export default Home
+    </div>
+  );
+};
+export default Home;
